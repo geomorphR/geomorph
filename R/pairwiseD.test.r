c714c1dd921f3c0fed434d2115f97026f188d52b
Michael Collyer <mlcollyer@gmail.com>
2015-03-29 10:35:56 -0500

    Update pairwiseD.test.r

--- R/pairwiseD.test.r
+++ R/pairwiseD.test.r
@@ -9,26 +9,27 @@
 #'  
 #'  As input the user provides a formula describing the linear model of how shape (y) varies as a function of a factor (a) 
 #'  or factorial interaction (a*b). A single covariate, matrix of covariates, or data frame of covariates can also be added.
-#'  E.g., covariates = x1, covariates = cbind(x1, x2, x3,...), or covariates = data.frame(x1, x2, x3,...).  Shape data (y)
-#'  can be either in the form of a two-dimensional data matrix of dimension (n x [p x k]), or a 3D array (p x n x k). 
+#'  E.g., covariates = x1, covariates = cbind(x1, x2, x3,...), or covariates = data.frame(x1, x2, x3,...).
+#'  The shape data (y) must be in the form of a two-dimensional data matrix of dimension (n x [p x k]), rather than a 3D array.  
 #'  It is assumed that the landmarks have previously been aligned using Generalized Procrustes Analysis (GPA) 
-#'  [e.g., with \code{\link{gpagen}}]. From the data, the Euclidean distances among group means are estimated, and used 
+#'  [e.g., with \code{\link{gpagen}}]. The function \code{\link{two.d.array}} can be used to obtain a two-dimensional data matrix 
+#'  from a 3D array of landmark coordinates. From the data, the Euclidean distances among group means are estimated, and used 
 #'  as test values.
 #'   
 #'   To evaluate significance of group differences, two possible resampling procedures are provided. First, if 
 #'   RRPP=FALSE, the rows of the matrix of shape variables are randomized relative to the design matrix. This is 
 #'   analogous to a 'full' randomization. Second, if RRPP=TRUE, a residual randomization permutation procedure 
-#'   is utilized (Collyer et al. 2015). Here, residual shape values from a reduced model are
+#'   is utilized (Collyer et al. 2014). Here, residual shape values from a reduced model are
 #'   obtained, and are randomized with respect to the linear model under consideration. These are then added to 
 #'   predicted values from the remaining effects to obtain pseudo-values from which SS are calculated. NOTE: for
 #'   single-factor designs, the two approaches are identical.  However, when evaluating factorial models it has been
 #'   shown that RRPP attains higher statistical power and thus has greater ability to identify patterns in data should
 #'   they be present (see Anderson and terBraak 2003). Effect-sizes (Z-scores) are computed as standard deviates of the SS 
-#'   sampling distributions generated, which might be more intuitive for P-values than F-values (see Collyer et al. 2015).
+#'   sampling distributions generated, which might be more intuitive for P-values than F-values (see Collyer et al. 2014).
 #'   
 #'   This test is essentially the same as procD.lm with post-hoc comparisons among least squares (LS) means for appropriate
 #'   models.  However, differences in means are calculated simultaneously with the same random permutations peformed for ANOVA,
-#'   making it less so a post-hoc test and more so a simultaneous test of pairwise contrasts (see Collyer et al. 2015).
+#'   making it less so a post-hoc test and more so a simultaneous test of pairwise contrasts (see Collyer et al. 2014).
 #'
 #' @param f1 A formula for the linear model from which groups are to be compared (e.g., y~x1*x2)
 #' @param f2 A right-side formula for one or more covariates (e.g., ~ CS + altitude)
@@ -48,7 +49,7 @@
 #' @examples
 #' data(plethodon) 
 #' Y.gpa<-gpagen(plethodon$land)    #GPA-alignment    
-#' y <- Y.gpa$coords 
+#' y<-two.d.array(Y.gpa$coords)
 #' ### Procrustes ANOVA
 #' procD.lm(y~plethodon$species,iter=99)
 #' 
@@ -66,14 +67,16 @@
 #' 
 pairwiseD.test <- function(f1, f2 = NULL, RRPP = FALSE, int.first = FALSE, iter= 999){
   f1 <- as.formula(f1)
-  Y <- eval(f1[[2]], parent.frame())
-  if(length(dim(Y)) == 3)  Y <- two.d.array(Y) else Y <- as.matrix(Y)
-  f1 <- as.formula(paste(c("Y",f1[[3]]),collapse="~"))
   if(int.first == TRUE) ko = TRUE else ko = FALSE
   fTerms <- terms(as.formula(f1), keep.order = ko)
   fac.mf <- model.frame(f1)
-  if(any(is.na(Y)) == T) stop("Response data matrix (shape) contains missing values. Estimate these first (see 'estimate.missing').")
-  if(nrow(Y) != nrow(fac.mf)) stop("Different numbers of specimens in dependent and independent variables")
+  Y <- as.matrix(fac.mf[1])
+  if (length(dim(Y)) != 2) {
+    stop("Response matrix (shape) not a 2D array. Use 'two.d.array' first.")
+  }
+  if (any(is.na(Y)) == T) {
+    stop("Response data matrix (shape) contains missing values. Estimate these first (see 'estimate.missing').")
+  }
   Xfacs <- model.matrix(fTerms)
   newfac <- single.factor(f1, keep.order = ko)
   if(is.null(f2)){
@@ -82,8 +85,8 @@ pairwiseD.test <- function(f1, f2 = NULL, RRPP = FALSE, int.first = FALSE, iter=
   if(any(Xfacs != 1 & Xfacs != 0)) stop("Only factors allowed as independent variables in model formula. 
                                         \nMake sure covariates are input separately. \ne.g., f2 = ~ X1 + X2 +...")
   if(ncol(model.matrix(~newfac)) != ncol(Xfacs)) stop("Model formula must be for either a single factor or full-factorial model\n  e.g., Shape ~ Factor.A  -or-  Shape ~ Factor.A * Factor.B * ...")
-  if(is.null(f2)) {Xs <-mod.mats(fac.mf, keep.order=ko) 
-  } else {Xs <- mod.mats.w.cov(fac.mf, cov.mf, keep.order=ko)}
+  if(is.null(f2)) {Xs <-mod.mats(f1, dat1=fac.mf, keep.order=ko) 
+  } else {Xs <- mod.mats.w.cov(f1, f2, dat1=fac.mf, dat2=cov.mf, keep.order=ko)}
   k <- length(Xs$Xs) - 1
   X <- Xs$Xs[[k+1]]
   anova.parts.obs <- anova.parts(f1, X = Xs,Yalt = "observed", keep.order=ko)
