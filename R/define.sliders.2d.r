2ee3d0082a07c2d5b065465f9fe2e85aaab44ec8
EmSherratt <emma.sherratt@gmail.com>
2015-04-13 16:38:50 +1000

    Deprecating define.sliders.2d and 3d

---
+++ R/define.sliders.2d.r
@@ -0,0 +1,14 @@
+#' Deprecated functions in geomorph
+#'
+#' The following functions have been deprecated in geomorph
+#'
+#' This function has been deprecated. Below shows the original function and the replacement function.
+#'
+#' define.sliders.2d: use define.sliders
+#'
+#' define.sliders.3d: use define.sliders
+#' @export
+define.sliders.2d<-function()    {
+  .Deprecated(define.sliders)
+}
+
---
+++ R/define.sliders.3d.r
@@ -0,0 +1,13 @@
+#' Deprecated functions in geomorph
+#'
+#' The following functions have been deprecated in geomorph
+#'
+#' This function has been deprecated. Below shows the original function and the replacement function.
+#'
+#' define.sliders.2d: use define.sliders
+#'
+#' define.sliders.3d: use define.sliders
+#' @export
+define.sliders.3d<-function()    {
+  .Deprecated(define.sliders)
+}
