618c182d7541fecd398e0be0291a70f3eedc19c1
Michael Collyer <mlcollyer@gmail.com>
2015-05-19 20:09:00 -0500

    Update two.b.pls.r

--- R/two.b.pls.r
+++ R/two.b.pls.r
@@ -35,7 +35,7 @@
 #'
 #' #2B-PLS between head shape and food use data
 #' two.b.pls(Y.gpa$coords,plethShapeFood$food,iter=99)
-#' 
+#'
 two.b.pls<- function (A1, A2, warpgrids = TRUE, iter = 999, verbose = FALSE, label = NULL,ShowPlot=TRUE){
     if (any(is.na(A1)) == T) {
       stop("Data matrix 1 contains missing values. Estimate these first (see 'estimate.missing').")
@@ -181,8 +181,9 @@ two.b.pls<- function (A1, A2, warpgrids = TRUE, iter = 999, verbose = FALSE, lab
     }
 
     if (verbose == TRUE) {
-      return(list(x.scores = XScores[, 1], y.scores = YScores[, 
-                                                              1], PLS.corr = PLS.obs, pvalue = P.val, pls.random = pls.val))
+      return(list(x.scores = XScores[, 1], y.scores = YScores[,1], 
+                  PLS.corr = PLS.obs, pvalue = P.val, pls.random = pls.val,
+                  left.singular.coef=pls.xy$U[,1], right.singular.coef=pls.xy$V[,1]))
     }
     if (verbose == FALSE) {
       return(list(PLS.corr = PLS.obs, pvalue = P.val))
