cce860ee25d242a9416fc328a3c758558c3dd2d8
EmSherratt <emma.sherratt@gmail.com>
2015-05-21 10:38:48 +1000

    Updated so all deprecated functions have same format

--- R/define.sliders.2d.r
+++ R/define.sliders.2d.r
@@ -1,12 +1,10 @@
 #' Deprecated functions in geomorph
 #'
-#' The following functions have been deprecated in geomorph
+#' The following function has been deprecated in geomorph
 #'
 #' This function has been deprecated. Below shows the original function and the replacement function.
 #'
-#' define.sliders.2d: use define.sliders
-#'
-#' define.sliders.3d: use define.sliders
+#' define.sliders.2d: use define.sliders instead
 #' @export
 define.sliders.2d<-function()    {
   .Deprecated(define.sliders)
--- R/pairwiseD.test.r
+++ R/pairwiseD.test.r
@@ -1,6 +1,10 @@
-#' PairwiseD.test (deprecated)
+#' Deprecated functions in geomorph
+#'
+#' The following function has been deprecated in geomorph
+#'
+#' This function has been deprecated. Below shows the original function and the replacement function.
 #' 
-#' Function deprecated: use 'advanced.procD.lm' instead
+#' pairwiseD.test: use 'advanced.procD.lm' instead
 #' @export
 pairwiseD.test <- function(){
   .Deprecated("advanced.procD.lm")
