9cfab884884c1cf00141e6001ab5f717fd322ed8
Michael Collyer <mlcollyer@gmail.com>
2016-01-06 12:52:13 -0600

    updated p-values

--- R/phylo.modularity.R
+++ R/phylo.modularity.R
@@ -121,8 +121,7 @@ phylo.modularity<-function(A,partition.gp,phy,iter=999, seed=NULL){
     CR.rand <- apply.phylo.CR(x, invC,gps, iter=iter, seed=seed)
     CR.rand[1] <- CR.obs <- avgCR
     if(ngps > 2) CR.mat <- CR(x,gps)$CR.mat else CR.mat <- NULL
-    p.val <- 1-pval(CR.rand)  #b/c smaller values more significant
-    if (p.val==0){p.val<-1/(iter+1)}
+    p.val <- pval(1/CR.rand)  #b/c smaller values more significant
     CR.boot<- boot.phylo.CR(x, invC=invC, gps=gps, iter=iter, seed=seed)
     CR.CI<-quantile(CR.boot, c(.025, .975))
   }
