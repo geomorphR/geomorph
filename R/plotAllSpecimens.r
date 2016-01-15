72cb85bf725a267c5ef1f10e24529c5710351bf2
EmSherratt <emma.sherratt@gmail.com>
2016-01-16 09:33:17 +1000

    Updated plotAllSpecimens plotting to match plotGMPhyloMorphoSPace

--- R/plotAllSpecimens.r
+++ R/plotAllSpecimens.r
@@ -7,23 +7,13 @@
 #'  Additionally, if a matrix of links is provided, the landmarks of the mean shape will be connected by lines.  
 #'  The link matrix is an m x 2 matrix, where m is the desired number of links. Each row of the link matrix 
 #'  designates the two landmarks to be connected by that link. The function will plot either two- or 
-#'  three-dimensional data.
+#'  three-dimensional data (e.g. see \code{\link{define.links}}).
 #'
 #' @param A An array (p x k x n) containing GPA-aligned coordinates for a set of specimens
 #' @param mean A logical value indicating whether the mean shape should be included in the plot
 #' @param links An optional matrix defining for links between landmarks (only if mean=TRUE)
 #' @param label A logical value indicating whether landmark numbers will be plotted (only if mean=TRUE)
-#' @param pt.bg An optional value defining the background color of points (single value or vector of values)
-#' @param pt.cex An optional value defining the the size of the points (single value or vector of values)
-#' @param mean.bg An optional value defining the background color of the points for all specimens
-#' @param mean.cex An optional value defining the size of the points representing the average specimen
-#' @param link.col An optional value defining color of links (single value or vector of values)
-#' @param link.lwd An optional value defining line weight of links (single value or vector of values)
-#' @param link.lty An optional value defining line type of links (single value or vector of values)
-#' @param txt.adj The adjustment value of the landmark label (one or two values, as in base R \code{\link{text}}) 
-#' @param txt.pos The position of the landmark label (single numerical value, as in base R \code{\link{text}}) 
-#' @param txt.cex The size of the landmark label text (single numerical value, as in base R \code{\link{text}})
-#' @param txt.col The color of the landmark label text (single numerical value, as in base R \code{\link{text}})
+#' @param plot.param A list of plotting parameters for the points (pt.bg, pt.cex), mean (mean.bg, mean.cex), links (link.col, link.lwd, link.lty) and landmark labels (txt.cex, txt.adj, txt.pos, txt.col)
 #' @export
 #' @keywords visualization
 #' @author Dean Adams
@@ -32,18 +22,7 @@
 #' Y.gpa<-gpagen(plethodon$land)    #GPA-alignment
 #'
 #' plotAllSpecimens(Y.gpa$coords,links=plethodon$links)
-plotAllSpecimens<-function(A,mean=TRUE,links=NULL,label=FALSE,
-                           pt.bg="gray",
-                           pt.cex=1,
-                           mean.bg="black",
-                           mean.cex=2,
-                           link.col="black",
-                           link.lwd = 2,
-                           link.lty = 1,
-                           txt.adj = 0.5,
-                           txt.pos = 1, 
-                           txt.cex = 0.8,
-                           txt.col = "black"){
+plotAllSpecimens<-function(A,mean=TRUE,links=NULL,label=FALSE,plot.param = list()){
   if (length(dim(A))!=3){
     stop("Data matrix not a 3D array (see 'arrayspecs').")  }
   if(any(is.na(A))==T){
@@ -52,21 +31,30 @@ plotAllSpecimens<-function(A,mean=TRUE,links=NULL,label=FALSE,
   if(mean==TRUE){
     mn<-mshape(A)
   }
+  
+  p.p <- plot.param
+  if(is.null(p.p$pt.bg)) p.p$pt.bg="gray" ; if(is.null(p.p$pt.cex)) p.p$pt.cex=1 ; 
+  if(is.null(p.p$mean.bg)) p.p$mean.bg="black" ; if(is.null(p.p$mean.cex)) p.p$mean.cex=2
+  if(is.null(p.p$link.col)) p.p$l.col="black" ; if(is.null(p.p$link.lwd)) p.p$link.lwd=2
+  if(is.null(p.p$link.lty)) p.p$link.lty=1 ; if(is.null(p.p$txt.adj)) p.p$txt.adj=c(-.1,-.1)
+  if(is.null(p.p$txt.col)) p.p$txt.col="black" ; if(is.null(p.p$txt.cex)) p.p$txt.cex=0.8
+  if(is.null(p.p$txt.pos)) p.p$txt.pos=1
+  
   if(k==2){
-    plot(A[,1,],A[,2,],asp=1, pch=21,bg=pt.bg,cex=pt.cex*1,xlab="x",ylab="y") 
+    plot(A[,1,],A[,2,],asp=1, pch=21,bg=p.p$pt.bg,cex=p.p$pt.cex*1,xlab="x",ylab="y") 
     if(mean==TRUE){ 
       if(is.null(links)==FALSE){
-        linkcol <- rep(link.col,nrow(links))[1:nrow(links)]
-        linklwd <- rep(link.lwd,nrow(links))[1:nrow(links)]
-        linklty <- rep(link.lty,nrow(links))[1:nrow(links)]
+        linkcol <- rep(p.p$link.col,nrow(links))[1:nrow(links)]
+        linklwd <- rep(p.p$link.lwd,nrow(links))[1:nrow(links)]
+        linklty <- rep(p.p$link.lty,nrow(links))[1:nrow(links)]
         for (i in 1:nrow(links)){
           segments(mn[links[i,1],1],mn[links[i,1],2],mn[links[i,2],1],mn[links[i,2],2],
                    col=linkcol[i],lty=linklty[i],lwd=linklwd[i])
         }
       }
-      points(mn,pch=21,bg=mean.bg,cex=mean.cex)
-      if(label == TRUE){text(mn, label=paste(1:dim(mn)[1]),adj=(txt.adj+mean.cex),
-                             pos=txt.pos,cex=txt.cex,col=txt.col)}
+      points(mn,pch=21,bg=p.p$mean.bg,cex=p.p$mean.cex)
+      if(label == TRUE){text(mn, label=paste(1:dim(mn)[1]),adj=(p.p$txt.adj+p.p$mean.cex),
+                             pos=p.p$txt.pos,cex=p.p$txt.cex,col=p.p$txt.col)}
     }
   }
   if(k==3){
@@ -74,7 +62,7 @@ plotAllSpecimens<-function(A,mean=TRUE,links=NULL,label=FALSE,
     for (i in 1:dim(A)[[3]]){
       A3d<-rbind(A3d,A[,,i])
     }
-    plot3d(A3d,type="s",col=pt.bg,xlab="x",ylab="y",zlab="z",size=pt.cex*1.5,aspect=FALSE)
+    plot3d(A3d,type="s",col=p.p$pt.bg,xlab="x",ylab="y",zlab="z",size=p.p$pt.cex*1.5,aspect=FALSE)
     if(mean==TRUE){ 
       if(is.null(links)==FALSE){
         linkcol <- rep(link.col,nrow(links))[1:nrow(links)]
@@ -85,9 +73,9 @@ plotAllSpecimens<-function(A,mean=TRUE,links=NULL,label=FALSE,
                      col=linkcol[i],lty=linklty[i],lwd=linklwd[i])
         }
       }
-      points3d(mn,color=mean.bg,size=mean.cex*2)
-      if(label == TRUE){text3d(mn, texts = paste(1:dim(mn)[1]), adj=(txt.adj+mean.cex),
-                               pos=txt.pos,cex=txt.cex,col=txt.col)}
+      points3d(mn,color=p.p$mean.bg,size=p.p$mean.cex*2)
+      if(label == TRUE){text3d(mn, texts = paste(1:dim(mn)[1]), adj=(p.p$txt.adj+p.p$mean.cex),
+                               pos=p.p$txt.pos,cex=p.p$txt.cex,col=p.p$txt.col)}
     }
   }
 }
