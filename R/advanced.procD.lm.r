afd3a91d2f6236dadfb2748bac2f022515314cec
Michael Collyer <mlcollyer@gmail.com>
2016-01-20 11:59:21 -0600

    fixed a distance matrix compatibility issue

--- R/advanced.procD.lm.r
+++ R/advanced.procD.lm.r
@@ -135,7 +135,7 @@ advanced.procD.lm<-function(f1, f2, groups = NULL, slope = NULL,
     pairwise.cond <-"none"
   }
   data.types <- lapply(data, class)
-  keep = sapply(data.types, function(x) x != "array" & x != "phylo")
+  keep = sapply(data.types, function(x) x != "array" & x != "phylo"  & x != "dist")
   if(!is.null(data)) dat2 <- as.data.frame(data[keep]) else dat2 <- pfitf$data
     
   if(!is.null(groups)){
