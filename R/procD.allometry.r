f3ace6cd2974a1c1576fcf1620426c57170bc4e3
Michael Collyer <mlcollyer@gmail.com>
2016-05-16 05:59:43 -0500

    fixed faulty HOS test and subseqnet correct model output

--- R/procD.allometry.r
+++ R/procD.allometry.r
@@ -42,32 +42,7 @@
 #' \subsection{Notes for geomorph 3.0 and making allometry plots}{ 
 #' Former versions of geomorph had a "plotAllometry" function that performed ANOVA and produced
 #' plots of allometry curves.  In geomorph 3.0, the \code{\link{plot}} function is used with 
-#' \code{\link{procD.allometry}} objects to produce such plots.  The following arguments can be used in 
-#' \code{\link{plot}} to achieve desired results.
-#' \itemize{
-#' \item{method = ("CAC, "RegScore, "PredLine").  Choose the desired plot method.}
-#' \item{warpgrids: default = TRUE.  Logical value to indicate whether warpgrids should be plotted.} 
-#' (Only workds with 3D array data)
-#' \item{label: can be logical to label points (1:n) - e.g., label = TRUE - or a vector indicating
-#' text to use as labels.}
-#' \item{mesh: A mesh3d object to be warped to represent shape deformation of the minimum and maximum size 
-#' if {warpgrids=TRUE} (see \code{\link{warpRefMesh}}).}
-#' }
-#' Use ?\code{\link{plot.procD.allometry}} to understand the arguments used.  The following are brief 
-#' descriptions of the different plotting methods using \code{\link{plot}}, with references.
-#'\itemize{
-#' \item {If "method=CAC" (the default) the function calculates the 
-#'   common allometric component of the shape data, which is an estimate of the average allometric trend 
-#'   within groups (Mitteroecker et al. 2004). The function also calculates the residual shape component (RSC) for 
-#'   the data.}
-#'   \item {If "method=RegScore" the function calculates shape scores 
-#'   from the regression of shape on size, and plots these versus size (Drake and Klingenberg 2008). 
-#'   For a single group, these shape scores are mathematically identical to the CAC (Adams et al. 2013).}
-#'   \item {If "method=PredLine" the function calculates predicted values from a regression of shape on size, and 
-#'   plots the first principal component of the predicted values versus size as a stylized graphic of the 
-#'   allometric trend (Adams and Nistri 2010). }
-#'   }
-#'   }
+#' \code{\link{procD.allometry}} objects to produce such plots. Use ?\code{\link{plot.procD.allometry}} to see the arguments.}
 #'   
 #' @param f1 A formula for the relationship of shape and size; e.g., Y ~ X.
 #' @param f2 An optional right-hand formula for the inclusion of groups; e.g., ~ groups.
@@ -102,23 +77,15 @@
 #' \item{Reg.proj}{The projected regression scores on the regression of shape on size. 
 #'   For a single group, these shape scores are mathematically identical to the CAC (Adams et al. 2013).}
 #' \item{pred.val}{Principal component scores (first PC) of predicted values.}
-#' \item{ref}{the reference configuration (if input coordinates are in a 3D array).}
+#' \item{ref}{the reference configuration (if input coordinates are in a 3D array), or variable means (if input data are in a 2D matrix).}
 #' \item{gps}{A vector of group names.}
 #' \item{size}{A vector of size scores.}
 #' \item{logsz}{A logical value to indicate if size values were log=transformed for analysis.}
-#' \item{A}{Procrustes (aligned) residuals.}
-#' \item{Ahat}{Predicted Procrustes residuals(if input coordinates are in a 3D array).}
-#' \item{p}{landmark number}
-#' \item{k}{landmark dimensions}
+#' \item{A}{Procrustes (aligned) residuals (if input coordinates are in a 3D array), or residuals (if input data are in a 2D matrix). }
+#' \item{Ahat}{Predicted Procrustes residuals (if input coordinates are in a 3D array), or predicted values (if input data are in a 2D matrix).}
+#' \item{p}{landmark number (if input coordinates are in a 3D array), or number of variables (if input data are in a 2D matrix).}
+#' \item{k}{landmark dimensions (if input coordinates are in a 3D array).}
 #' 
-#' @references Adams, D.C., F.J. Rohlf, and D.E. Slice. 2013. A field comes of age: geometric morphometrics 
-#'   in the 21st century. Hystrix. 24:7-14. 
-#' @references Adams, D. C., and A. Nistri. 2010. Ontogenetic convergence and evolution of foot morphology 
-#'   in European cave salamanders (Family: Plethodontidae). BMC Evol. Biol. 10:1-10.
-#' @references Drake, A. G., and C. P. Klingenberg. 2008. The pace of morphological change: Historical 
-#'   transformation of skull shape in St Bernard dogs. Proc. R. Soc. B. 275:71-76.
-#' @references Mitteroecker, P., P. Gunz, M. Bernhard, K. Schaefer, and F. L. Bookstein. 2004. 
-#'   Comparison of cranial ontogenetic trajectories among great apes and humans. J. Hum. Evol. 46:679-698.
 #' @seealso \code{\link{procD.lm}} and \code{\link{advanced.procD.lm}} within geomorph;
 #' \code{\link[stats]{lm}} for more on linear model fits
 #' @examples
@@ -154,7 +121,7 @@
 #' summary(plethANOVA) # Same ANOVA
 #' plot(plethANOVA) # diagnostic plot instead of allometry plot
 procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
-                   iter = 999, seed=NULL, alpha = 0.05, RRPP = TRUE, data=NULL, ...){
+                           iter = 999, seed=NULL, alpha = 0.05, RRPP = TRUE, data=NULL, ...){
   pfit <- procD.fit(f1, data=data, pca=FALSE)
   dat <- pfit$data
   Y <- pfit$Y
@@ -175,8 +142,8 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
       data.types <- lapply(data, class)
       keep = sapply(data.types, function(x) x != "array" & x != "phylo" & x != "dist")
       dat2 <- as.data.frame(data[keep])
-      } else dat2 <- NULL
-      
+    } else dat2 <- NULL
+    
     if(!is.null(f2)) {
       if(length(f2) > 2) f2 <- f2[-2]
       dat.g <- model.frame(f2, data=dat2) 
@@ -185,12 +152,14 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
       if(any(attr(g.Terms, "dataClasses") == "numeric")) stop("groups formula (f2) must contain only factors")
       if(ncol(dat.g) > 1) gps <- factor(apply(dat.g, 1,function(x) paste(x, collapse=":"))) else 
         gps <- as.factor(unlist(dat.g))
+      form2 <- update(form1, ~. + gps)
     }  else {
       dat.g <- NULL
       g.Terms <- NULL
       gps <- NULL
+      form2 <- form1
     }
-  
+    
     if(!is.null(f3)) {
       if(length(f3) > 2) f3 <- f3[-2]
       dat.o <- model.frame(f3, data=dat2) 
@@ -201,21 +170,22 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
       o.Terms <- NULL
     }
   }
-
-    if(!is.null(f2) & !is.null(f3)) {
-      if(!logsz){
-        form4 <- update(f3, ~. + size + gps)
-        form5 <- update(f3, ~. + size * gps)
-      }
-      if(logsz){
-        form4 <- update(f3, ~. + log(size) + gps)
-        form5 <- update(f3, ~. + log(size) * gps)
-      }
+  
+  if(!is.null(f2) & !is.null(f3)) {
+    if(!logsz){
+      form4 <- update(f3, ~. + size + gps)
+      form5 <- update(f3, ~. + size * gps)
     }
-      if(!is.null(f2) & is.null(f3)) {
-        form4 <- form1
-        form5 <- update(form1, ~.  * gps)
-      }
+    if(logsz){
+      form4 <- update(f3, ~. + log(size) + gps)
+      form5 <- update(f3, ~. + log(size) * gps)
+    }
+  }
+  
+  if(!is.null(f2) & is.null(f3)) {
+    form4 <- form2
+    form5 <- update(form1, ~.  * gps)
+  }
   
   if(!is.null(f2) & !is.null(f3)) {
     formfull <-as.formula(c("~",paste(unique(
@@ -230,7 +200,7 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
         formfull <-as.formula(c("~",paste(unique(
           c(c("log(size)", attr(g.Terms, "term.labels"), paste("log(size)", attr(g.Terms, "term.labels"), sep=":")))),
           collapse="+")))
-    form.type <- "g"
+      form.type <- "g"
   } else if(is.null(f2) & !is.null(f3)) {
     if(!logsz) formfull <-as.formula(c("~",paste(unique(
       c(c("size", attr(o.Terms, "term.labels"), paste("size", attr(o.Terms, "term.labels"), sep=":")))),
@@ -238,9 +208,9 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
         formfull <-as.formula(c("~",paste(unique(
           c(c("log(size)", attr(o.Terms, "term.labels"), paste("log(size)", attr(o.Terms, "term.labels"), sep=":")))),
           collapse="+")))
-    form.type <- "o"
+      form.type <- "o"
   } else {
-    formfull <- form1
+    formfull <- form2
     form.type <- NULL}
   
   if(!is.null(f2)){
@@ -254,9 +224,9 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
       if(form.type == "go") {
         if(!logsz) rhs.formfull <- paste(c("size", attr(g.Terms, "term.labels"), 
                                            attr(o.Terms, "term.labels")), collapse="+") else
-                   rhs.formfull <- paste(c("log(size)", attr(g.Terms, "term.labels"), 
-                                           attr(o.Terms, "term.labels")), collapse="+")  
-        formfull <- as.formula(c("Y ~", rhs.formfull))
+                                             rhs.formfull <- paste(c("log(size)", attr(g.Terms, "term.labels"), 
+                                                                     attr(o.Terms, "term.labels")), collapse="+")  
+                                           formfull <- as.formula(c("Y ~", rhs.formfull))
       }
       if(form.type == "g") {
         if(!logsz) rhs.formfull <- paste(c("size", attr(g.Terms, "term.labels")),  collapse="+") else
@@ -286,9 +256,16 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
     Z <- eval(f1[[2]], parent.frame())
     lm.dim <- dim(Z)
   }
-  Ahat <- arrayspecs(yhat, lm.dim[[1]], lm.dim[[2]])
-  A <- arrayspecs(Y, lm.dim[[1]], lm.dim[[2]])
-  ref<-mshape(A)
+  if(lm.dim[[2]] == 2 || lm.dim[[2]] == 3){
+    Ahat <- arrayspecs(yhat, lm.dim[[1]], lm.dim[[2]])
+    A <- arrayspecs(Y, lm.dim[[1]], lm.dim[[2]])
+    ref<-mshape(A)
+    p=lm.dim[[1]] ; k= lm.dim[[2]]
+  } else {
+    Ahat <- yhat ; A <- Y
+    ref<-apply(A, 2, mean)
+    p= lm.dim[[2]] ; k=NULL
+  }
   if(is.null(f2)) gps <- NULL
   out <- list(HOS.test = HOS, aov.table =anovafull, call = match.call(),
               alpha = alpha, perm.method = perm.method, permutations=iter+1,
@@ -296,7 +273,7 @@ procD.allometry<- function(f1, f2 = NULL, f3 = NULL, logsz = TRUE,
               CAC = CAC, RSC=RSC, Reg.proj = Reg.proj,
               pred.val=pred.val,
               ref=ref, gps=gps, size=size, logsz=logsz, 
-              A=A, Ahat=Ahat, p=lm.dim[[1]], k= lm.dim[[2]])
+              A=A, Ahat=Ahat, p=p, k=k)
   class(out) <- "procD.allometry"
   out
 }
