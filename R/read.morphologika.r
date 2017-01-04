c34670647a5db92e1b12527812190f82e818c3db
Emma Sherratt <emma.sherratt@gmail.com>
2017-01-04 15:38:17 +1100

    Update to read.morphologika to read multiple files

--- R/read.morphologika.r
+++ R/read.morphologika.r
@@ -1,18 +1,24 @@
-#'  Read landmark data from Morphologika file
+#'  Read landmark data from Morphologika file(s)
 #'
 #'  Read Morphologika file (*.txt) to obtain landmark coordinates and specimen information
 #'
 #'  This function reads a *.txt file in the Morphologika format containing two- or three-dimensional 
 #'  landmark coordinates. Morphologika files are text files in one of the standard formats for 
-#'  geometric morphometrics (see O'Higgins and Jones 1998,2006). 
+#'  geometric morphometrics (see O'Higgins and Jones 1998,2006). If multiple morphologika files 
+#'  are specified (containing the same number of landmarks for all specimens), function returns a single object for files.
 #' 
 #'  If the headers "[labels]", "[labelvalues]" and "[groups]" are present, then a data matrix containing all 
 #'  individual specimen information is returned.
 #'  If the header "[wireframe]" is present, then a matrix of the landmark addresses for the wireframe is
 #'  returned (see \code{\link{plotRefToTarget}} option 'links'). 
 #'  If the header "[polygon]" is present, then a matrix of the landmark addresses for the polygon wireframe is returned (see \code{\link[rgl]{polygon3d}} or \code{\link[graphics]{polygon}}). 
+#'  
+#'  NOTE: For multiple morphologika files that each contain only a single specimen (such as those exported from
+#'  Stratovan Checkpoint software https://www.stratovan.com/products/checkpoint), one can add specimen names to 
+#'  the returned 3D array by: dimnames(mydata)[[3]] <- gsub (".txt", "", filelist)).
 #' 
-#' @param file A Morphologika *.txt file containing two- or three-dimensional landmark data. 
+#' @param filelist The name of a Morphologika *.txt file containing two- or three-dimensional landmark data.
+#' Alternatively, a character vector of names of Morphologika *.txt file (such as made using \code{\link{list.files}}) 
 #' @export
 #' @keywords IO
 #' @author Emma Sherratt & Erik Otarola-Castillo
@@ -22,8 +28,35 @@
 #' @references O'Higgins P and Jones N (1998) Facial growth in Cercocebus torquatus: An application of three 
 #' dimensional geometric morphometric techniques to the study of morphological  variation. Journal of Anatomy. 
 #' 193: 251-272 
-#' @references O'Higgins P and Jones N (2006) Tools for statistical shape analysis. Hull York Medical School.   
-read.morphologika<-function(file){
+#' @references O'Higgins P and Jones N (2006) Tools for statistical shape analysis. Hull York Medical School.  
+read.morphologika<-function(filelist){
+  rtrn <- list()
+  if(length(filelist) == 1) rtrn <- read.one.morphologika(filelist)
+  if(length(filelist) > 1){
+    coords <- labvalmat <- wiref <- polyg<- NULL
+    for (i in 1:length(filelist)){
+      temp <- read.one.morphologika(filelist[i])
+      if(is.list(temp)){
+        p <- dim(temp$coords)[1]; k <- dim(temp$coords)[2]
+        coords <- rbind(coords, two.d.array(temp$coords)) 
+        if(!is.null(temp$labels)) labvalmat <- rbind(labvalmat, temp$labels) 
+        if(!is.null(temp$wireframe)) wiref <- temp$wireframe 
+        if(!is.null(temp$polygon)) polyg <- temp$polygon
+      } else {
+        p <- dim(temp)[1]; k <- dim(temp)[2]
+        coords <- rbind(coords, two.d.array(temp)) }
+    }
+    coords <- arrayspecs(coords, p, k) 
+    rtrn$coords <- coords
+    if(!is.null(labvalmat)) rtrn$labels <- labvalmat
+    if(!is.null(wiref)) rtrn$wireframe <- wiref
+    if(!is.null(polyg)) rtrn$polygon <- polyg
+    if(length(rtrn) == 1) rtrn <- rtrn$coords
+  }
+  return(rtrn)
+}  
+
+read.one.morphologika<-function(file){
   mfile<-scan(file=file,what="char",quote="",sep="\n",strip.white=TRUE,comment.char="\"",quiet=TRUE)
   if(length(grep("[",mfile, fixed=T))<1){
     stop("File does not appear to be a Morphologika format text file.")}
@@ -60,7 +93,7 @@ read.morphologika<-function(file){
   if(length(grep("wireframe",mfile,ignore.case=T))>0) {
     strtwf <- grep("wireframe",mfile, ignore.case=T)
     endwf <-strtwf + grep("[",mfile[strtwf+1:length(mfile)], fixed=T)[1]
-      if(is.na(endwf)){ endwf <- length(mfile)}
+    if(is.na(endwf)){ endwf <- length(mfile)}
     wiref <- matrix(as.numeric(unlist(strsplit(mfile[(strtwf+1):(endwf-1)],
                                                "\\s+"))),ncol=2, byrow=T) } 
   rtrn <- list(coords = coords)
@@ -69,4 +102,4 @@ read.morphologika<-function(file){
   if(!is.null(polyg)) rtrn$polygon <- polyg
   if(length(rtrn) == 1) rtrn <- rtrn$coords
   return(rtrn)
-}  
+}
