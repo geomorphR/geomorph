9e85f9b09969977b60b61d71caea87fc0b9456ef
mlcollyer <mlcollyer@gmail.com>
2017-02-05 11:13:39 -0500

    Enhanced landmark labeling in arrayspecs.

--- R/arrayspecs.r
+++ R/arrayspecs.r
@@ -11,6 +11,13 @@
 #' @param A A matrix containing landmark coordinates for a set of specimens
 #' @param p Number of landmarks
 #' @param k Number of dimensions (2 or 3)
+#' @param sep An optional argument to attempt to separate variable names into landmark dimension
+#' and landmark number variables.  For example, X.1, Y.1, Z.1, X.2, Y.2, Z.2, ..., can be separated 
+#' with sep = ".", such that rows of landmark configurations are labeled 1, 2, 3, ..., and columns
+#' are labeled X, Y, Z. Note, for variables, X1, Y1, Z1, X2, Y2, Z2, ...,
+#' where no separator is evident, use sep = "".  Any illogical separation argument will result in unlabeled
+#' variables.  If sep = NULL (the default), unlabeled variables are forced.  This is a good idea if the original matrix
+#' has landmarks out of order (as the the landmark labels might not sort as expected).
 #' @export
 #' @keywords utilities
 #' @author Dean Adams & Mike Collyer
@@ -24,7 +31,7 @@
 #'  
 #' x2<-matrix(rnorm(18),ncol=2) # Random triangles (each landmark on its own row)
 #' arrayspecs(x2,3,2)
-arrayspecs<-function(A,p,k){  
+arrayspecs<-function(A, p, k, sep = NULL){  
   if(!is.matrix(A) && !is.data.frame(A)) stop("A must be a data frame or matrix")
   dnames <- dimnames(A)
   n <- length(unlist(A))/(p * k)
@@ -33,15 +40,38 @@ arrayspecs<-function(A,p,k){
   specimens <- aperm(array(t(A), c(k,p,n)), c(2,1,3)) 
   dimnames(specimens)[[3]] <- dnames[[1]]
   col.names <- dnames[[2]]
+  if(is.null(sep)) col.names <- NULL
   if(!is.null(col.names)){
-    a <- strsplit(col.names, split = "[.]")
-    if(length(unlist(a)) == 2*k*p){
-      b <- simplify2array(a)
-      rn <- unique(b[1,])
-      cn <- unique(b[2,])
-    }  else b <- rn <- cn <- NULL
+    if(sep == ""){
+      options(warn = -1)
+      split.lab <-sort(unique(unlist(strsplit(col.names, split=""))))
+      split.lab <- split.lab[length(split.lab)]
+      a <- strsplit(col.names, split = split.lab)
+      a <- a[sapply(a, length) == 2]
+      a <- na.omit(as.numeric(unlist(a)))
+      if(length(unlist(a)) == p){
+        split.no <- as.character(a[length(a)])
+        b <- strsplit(col.names, split = split.no)
+        no.char <- sapply(b, nchar)
+        dim.tag <- which(no.char == min(no.char))
+        b <- unlist(b[dim.tag])
+        rn <- a
+        cn <- b
+      } else rn <- cn <- NULL
+      options(warn = 0)
+    } else{
+      sep = paste("[", noquote(sep), "]")
+      a <- strsplit(col.names, split = sep)
+      if(length(unlist(a)) == 2*k*p){
+        b <- simplify2array(a)
+        rn <- unique(b[1,])
+        cn <- unique(b[2,])
+        if(length(rn) < length(cn)) {tmp <- cn; cn <- rn; rn <- tmp}
+      }
+      else rn <- cn <- NULL
+    }
     dnames2 <- list(rn=rn, cn = cn)
   }
-  dimnames(specimens)[1:2] <- dnames2
+  if(!is.null(sep)) dimnames(specimens)[1:2] <- dnames2
   return(specimens)
 }
