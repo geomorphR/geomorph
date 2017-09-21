dff05e05e9ecae5d85b84c6959e6e023865addba
mlcollyer <mlcollyer@gmail.com>
2017-09-21 12:05:16 -0400

    fixed small issue with lm arguments in rrpp

--- R/geomorph.support.code.r
+++ R/geomorph.support.code.r
@@ -1382,10 +1382,11 @@ rrpp.w.o <- function(fitted, residuals, ind.i, w, o){
 }
 
 rrpp <- function(fitted, residuals, ind.i, w, o){
-  if(!is.null(w) && !is.null(o)) rrpp.w.o(fitted, residuals, ind.i, w, o)
-  if(!is.null(w) && is.null(o)) rrpp.w(fitted, residuals, ind.i, w)
-  if(is.null(w) && !is.null(o)) rrpp.o(fitted, residuals, ind.i, o)
-  if(is.null(w) && is.null(o)) rrpp.basic(fitted, residuals, ind.i)
+  if(!is.null(w) && !is.null(o)) r <- rrpp.w.o(fitted, residuals, ind.i, w, o)
+  if(!is.null(w) && is.null(o)) r <- rrpp.w(fitted, residuals, ind.i, w)
+  if(is.null(w) && !is.null(o)) r <- rrpp.o(fitted, residuals, ind.i, o)
+  if(is.null(w) && is.null(o)) r <- rrpp.basic(fitted, residuals, ind.i)
+  r
 }
 
 # SS.mean
