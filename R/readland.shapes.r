db276b589427ced4285c8aec16cea678c172ee2f
mlcollyer <mlcollyer@gmail.com>
2018-03-05 20:15:12 -0500

    added some traps and diversions for readland.shapes

--- R/readland.shapes.r
+++ R/readland.shapes.r
@@ -24,7 +24,9 @@
 #' nCurvePts = c(20, 50, 10) would return 20, 50, and 10 curve points, respectively.  If fixed landmarks are end points of curves,
 #' they will not be repeated as additional curve points.  Therefore, one should expect in these cases that the number of semilandmarks
 #' are fewer than the number of curve ponits; i.e., choosing 20 curve points might yield 18 semilandmarks.  If left NULL, no curve points
-#' will be estimated and only fixed landmarks will be read.
+#' will be estimated and only fixed landmarks will be read.  Note: if less than 3 curve points are chosen for an open curve or less than 4
+#' points are chosen for a closed curve, the number of points will default to 0, as fewer than these make it impossible to develop a curves 
+#' matrix.  A value of 0 can be chosen to omit specific curves when reading in data.
 #' @param continuous.curve An optional value or string of values to indicate which curves are closed and whose same start and end
 #' point should be treated as a semilandmark.  For example, if one wishes to digitize a curve around an eye, for digitizing purposes a point might
 #' be initiated to find a curve around the eye and back to the point.  This point will be "fixed" without indicating the curve is continuous.  One 
@@ -58,6 +60,14 @@
 #' #      continuous.curve = 2) # fixed landmarks plus curve points for three curves, one closed
 #' # myGPA <- gpagen(myGMdata, procD = FALSE) # GPA perfomed with minimized bending energy
 readland.shapes <- function(Shapes, nCurvePts = NULL, continuous.curve = NULL, scaled = TRUE){
-  if(is.null(nCurvePts)) GMfromShapes0(Shapes) else
-    GMfromShapes1(Shapes, nCurvePts = nCurvePts, continuous.curve = continuous.curve)
+  if(is.null(nCurvePts)) out <- GMfromShapes0(Shapes) else{
+    nCurvePts[nCurvePts < 3] = 0 # curves are either 3+ points or missing
+    if(!is.null(continuous.curve)) {
+      continuous.curve <- unlist(continuous.curve)
+      check <- which(nCurvePts[continuous.curve] < 4)
+      nCurvePts[check] = 0
+    }
+    out <- GMfromShapes1(Shapes, nCurvePts = nCurvePts, continuous.curve = continuous.curve)
+  }
+  out
 }
