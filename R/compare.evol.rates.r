10cfebbd594b9dc1e76ad24855daffaaf3ca567c
IASTATE\dcadams <dcadams@iastate.edu>
2019-03-24 21:03:21 -0500

    compare.evol.rates simulation fix

--- R/compare.evol.rates.r
+++ R/compare.evol.rates.r
@@ -107,7 +107,7 @@ compare.evol.rates<-function(A,phy,gp,iter=999,seed=NULL,method=c("simulation","
     diag(rate.mat)<-sigma.obs$sigma.d.all
     rate.mat<-makePD(rate.mat)
     x.sim<-simplify2array(sim.char.BM(phy=phy,par=rate.mat,nsim=iter, seed=seed)) 
-    x.r <- simplify2array(lapply(1:iter, function(j) Ptrans%*%x.sim[,,1]))
+    x.r <- simplify2array(lapply(1:iter, function(j) Ptrans%*%x.sim[,,j]))
   }
   if(method == "permutation"){
     ind<-perm.index(N,iter, seed=seed)
