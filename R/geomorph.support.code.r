a1147aa5f783ad685bd89061dd75528e82381ef0
IASTATE\dcadams <dcadams@iastate.edu>
2019-04-30 12:51:46 -0500

    phylo.mat

--- R/geomorph.support.code.r
+++ R/geomorph.support.code.r
@@ -1466,13 +1466,11 @@ phylo.mat<-function(x,phy){
   C<-C[rownames(x),rownames(x)]
   invC <-fast.solve(C)
   eigC <- eigen(C)
-  lambda <- zapsmall(abs(eigC$values))
+  lambda <- zapsmall(abs(Re(eigC$values)))
   if(any(lambda == 0)){
     warning("Singular phylogenetic covariance matrix. Proceed with caution")
-    lambda = lambda[lambda > 0]
   }
-  eigC.vect = eigC$vectors[,1:(length(lambda))]
-  D.mat <- fast.solve(eigC.vect%*% diag(sqrt(abs(lambda))) %*% t(eigC.vect))
+  D.mat <- fast.solve(eigC.vect%*% diag(sqrt(abs(eigC$values))) %*% t(eigC.vect))
   rownames(D.mat) <- colnames(D.mat) <- colnames(C)
   rownames(invC) <- colnames(invC) <- colnames(C)
   list(invC = invC, D.mat = D.mat,C = C)
@@ -2113,28 +2111,6 @@ readland.tps2 <- function (file, specID = c("None", "ID", "imageID"))
   return(list(coords = coords,scale=imscale)  )
 }
 
-# Function for retrieving pieces for PCA weighting from a VCV (not from phylo)
-# Follows phylo.mat
-
-cov.mat <- function(x, COV) {
-  if(is.null(dimnames(COV))){
-    warning("COV matrix does not include dimnames. Assuming same order of observations as x")
-  } else {
-    C <- C[rownames(x),rownames(x)]
-  }
-  invC <- fast.solve(C)
-  eigC <- eigen(C)
-  lambda <- zapsmall(abs(eigC$values))
-  if(any(lambda == 0)){
-    warning("Singular covariance matrix. Proceed with caution")
-    lambda = lambda[lambda > 0]
-  }
-  eigC.vect <- eigC$vectors[,1:(length(lambda))]
-  D.mat <- fast.solve(eigC.vect%*% diag(sqrt(abs(lambda))) %*% t(eigC.vect))
-  rownames(D.mat) <- colnames(D.mat) <- colnames(C)
-  rownames(invC) <- colnames(invC) <- colnames(C)
-  list(invC = invC, D.mat = D.mat, C = C)
-}
 
 ### All functions below perform in a constrained way the same as functions in ape and geiger:
 ### -----------------------------------------------------------------------------------------
