f7df80ce0bd64641b97db70d6918bb8e586fec1d
Antigoni Kaliontzopoulou <antigoni@cibio.up.pt>
2020-01-18 15:29:38 +0000

    Update to readmulti.tps function

--- R/readmulti.tps.R
+++ R/readmulti.tps.R
@@ -15,13 +15,17 @@
 #'   in the folder read. 
 #' @export
 
-readmulti.tps <- function(folder, ... ){
-  tps.list <- list.files(folder, pattern = c("[.tps]"))
+readmulti.tps <- function(filelist, ... ){
+  tps.list <- filelist
+  readland.args <- list(...)
+  if(is.null(readland.args$specID)) readland.args$specID <- "None"
   
-  if(length(tps.list)==0) stop("No tps files found in the indicated folder")
+  file.ext <- substr(tps.list, nchar(tps.list)-3, nchar(tps.list))
+  if(!all(file.ext%in%c(".tps", ".TPS"))) 
+    stop("File list includes files in a format other than tps, please ammend")
   
   dt.dims <- sapply(1:length(tps.list), function(x){
-    dim(readland.tps(paste(folder, tps.list[x], sep=""), warnmsg = F, ...))
+    dim(readland.tps(tps.list[x], warnmsg = F, ...))
   }, simplify = T)
   p1 <- dt.dims[1, 1]; k1 <- dt.dims[2, 1]; n1 <- dt.dims[3, 1]
   
@@ -31,9 +35,18 @@ readmulti.tps <- function(folder, ... ){
   
   all.lms <- NULL
   for(f in 1:length(tps.list)){       
-    lms <- two.d.array(readland.tps(paste(folder, tps.list[f], sep=""), specID = ...))
+    lms <- two.d.array(readland.tps(tps.list[f], ...))
     all.lms <- rbind(all.lms, lms)
   }
   all.lms <- arrayspecs(all.lms, p1, k1)
+  
+  if(any(table(dimnames(all.lms)[3])!=1)) {
+    if(readland.args$specID != "imageID") {
+      dimnames(all.lms)[[3]] <- 1:dim(all.lms)[3]
+    } else {
+      warning("Input files seem to include repeated specimen names")
+    }
+  }
+  
   return(all.lms)
 }
