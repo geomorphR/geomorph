8677a90c073eaf0ad95b3692d9a53a67cd97cab8
mlcollyer <mlcollyer@gmail.com>
2021-03-17 05:06:51 -0400

    removed defunct functions

--- R/deprecated.defunct.r
+++ R/deprecated.defunct.r
@@ -3,17 +3,3 @@
 #' The following function is now defunct
 #'
 #' 
-#' @export
-plotTangentSpace <- function(){
-  .Defunct("gm.prcomp")
-}
-
-#' Deprecated and defunct functions in geomorph
-#'
-#' The following function is now defunct
-#'
-#' 
-#' @export
-plotGMPhyloMorphoSpace <- function(){
-  .Defunct("gm.prcomp")
-}
