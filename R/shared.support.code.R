caedc333caf4f7417136505304bd74ecb441e1d0
IASTATE\dcadams <dcadams@iastate.edu>
2021-03-31 15:07:08 -0500

    anc.bm

--- R/shared.support.code.R
+++ R/shared.support.code.R
@@ -466,9 +466,9 @@ anc.BM <- function(phy, Y){
 
   if(NROW(out) == 1) out <- t(out)
   
-  if(dim(Y)[2]>1){
+#  if(dim(Y)[2]>1){
     dimnames(out) <- list(1:phy$Nnode + length(phy$tip.label), colnames(Y))
-  }else{  dimnames(out) <- list(1:phy$Nnode + length(phy$tip.label), NULL)}
+#  }else{  dimnames(out) <- list(1:phy$Nnode + length(phy$tip.label), NULL)}
   out
 }
 
