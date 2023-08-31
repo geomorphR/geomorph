4552e22af29607b76b2efde2ffdb7d7c62c2353c
Dean Adams <dcadams@iastate.edu>
2023-08-31 10:58:33 -0500

    3d plots

--- R/geomorph.support.code.r
+++ R/geomorph.support.code.r
@@ -1396,16 +1396,16 @@ tps2d3d <- function(M, matr, matt, PB=TRUE){		#DCA: altered from J. Claude 2008
 
 # In development for various functions
 
-model.matrix.g <- function(f1, data = NULL) {
-  f1 <- as.formula(f1)
-  Terms <- terms(f1)
-  labs <- attr(Terms, "term.labels")
-  if(!is.null(data)) {
-    matches <- na.omit(match(labs, names(data)))
-    dat <- as.data.frame(data[matches])
-  } else dat <- NULL
-  model.matrix(f1, data=dat)
-}
+#model.matrix.g <- function(f1, data = NULL) {
+#  f1 <- as.formula(f1)
+#  Terms <- terms(f1)
+#  labs <- attr(Terms, "term.labels")
+#  if(!is.null(data)) {
+#    matches <- na.omit(match(labs, names(data)))
+#    dat <- as.data.frame(data[matches])
+#  } else dat <- NULL
+#  model.matrix(f1, data=dat)
+#}
 
 # perm.CR.index
 # creates a permutation index for resampling, shuffling landmarks
