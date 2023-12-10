03100128580a08477d8bbce2b74cd8314f94de7a
mlcollyer <mlcollyer@gmail.com>
2023-12-10 10:22:26 -0500

    updated readmulti.tps

--- R/readmulti.tps.R
+++ R/readmulti.tps.R
@@ -5,12 +5,12 @@
 #' This is a wrapper of \code{\link{readland.tps}} to allow reading landmark coordinates, in 2D or 3D, from several tps files , and compiling them into an array for proceeding with GM procedures.
 #' 
 #' The arguments specID and negNA of \code{\link{readland.tps}} can be directly set through this function.
-#' Note that if specID is set to either "None" or "ID", a check for duplicate specimen names is not possible and specimens will be numbered with 1:N, where N the total number of specimens.  
 #' 
 #'
 #' @param filelist A vector containing the file paths to all the tps files to be compiled
 #' @param ... other arguments to be passed to \code{\link{readland.tps}}
 #' @author Antigoni Kaliontzopoulou
+#' @author Michael Collyer
 #' @return Function returns a (p x k x n) array, where p is the number of landmark points, k is the number 
 #'   of landmark dimensions (2 or 3), and n is the total number of specimens across all tps files included in the folder read. 
 #' @export
@@ -19,34 +19,49 @@ readmulti.tps <- function(filelist, ... ){
   tps.list <- filelist
   readland.args <- list(...)
   if(is.null(readland.args$specID)) readland.args$specID <- "None"
+  if(is.null(readland.args$negNA)) readland.args$negNA <- FALSE
+  if(is.null(readland.args$readcurves)) 
+    readland.args$readcurves <- FALSE
+  if(is.null(readland.args$warnmsg)) readland.args$warnmsg <- FALSE
   
   file.ext <- substr(tps.list, nchar(tps.list)-3, nchar(tps.list))
   if(!all(file.ext%in%c(".tps", ".TPS"))) 
     stop("File list includes files in a format other than tps, please ammend")
   
-  dt.dims <- sapply(1:length(tps.list), function(x){
-    dim(readland.tps(tps.list[x], warnmsg = F, ...))
-  }, simplify = T)
-  p1 <- dt.dims[1, 1]; k1 <- dt.dims[2, 1]; n1 <- dt.dims[3, 1]
+
+  readland.args$file <- tps.list[[1]]
+  all.lms <- lapply(1:length(tps.list), function(j){
+    readland.args$file <- tps.list[[j]]
+    do.call(readland.tps, readland.args)
+  })
   
-  if(any(dt.dims[1,]!=p1)) stop("Input tps files include different numbers of landmarks, please correct")
+  specnames <- sapply(all.lms, function(x) dimnames(x)[[3]])
+  all.lms <- lapply(all.lms, function(x) x[,,1] )
   
-  if(any(dt.dims[2,]!=k1)) stop("Input tps files include landmarks in different dimensions (2D and 3D), please correct")
+  if(all(specnames == "1")) specnames <- rep(NULL, length(specnames))
+  if(length(unique(specnames)) < length(specnames)) 
+    warning("\nInput files seem to include repeated specimen names.\n", 
+            call. = FALSE)
   
-  all.lms <- NULL
-  for(f in 1:length(tps.list)){       
-    lms <- two.d.array(readland.tps(tps.list[f], ...))
-    all.lms <- rbind(all.lms, lms)
-  }
-  all.lms <- arrayspecs(all.lms, p1, k1)
+  names(all.lms) <- specnames
+    
+  dt.dims <- sapply(all.lms, dim)
+  dim.unique <- apply(dt.dims, 1, unique)
   
-  if(any(table(dimnames(all.lms)[3])!=1)) {
-    if(readland.args$specID != "imageID") {
-      dimnames(all.lms)[[3]] <- 1:dim(all.lms)[3]
-    } else {
-      warning("Input files seem to include repeated specimen names")
+  bad.lm <- is.list(dim.unique)
+  
+  if(bad.lm) {
+    cat("\nInput tps files include either different numbers of landmarks\n")
+    cat("or different dimensions (2D or 3D) for some landmarks.\n\n")
+    cat("A table of dimensions is returned rather than an array, so the issue can be investigated.")
+    dt.dims <- dt.dims[1:2,]
+    rownames(dt.dims) <- c("points", "dimensions")
+    if(!all(is.na(specnames))) colnames(dt.dims) <- specnames
+    dt.dims <- as.table(dt.dims)
     }
-  }
   
-  return(all.lms)
+  if(!bad.lm)
+    all.lms <- simplify2array(all.lms)
+  
+  if(bad.lm) print(dt.dims) else return(all.lms)
 }
