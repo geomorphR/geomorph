a09699c94a20da97e3d539222441665db832644a
Dean Adams <dcadams@iastate.edu>
2024-06-21 16:40:38 +0200

    testthat started

--- tests/testthat/test-geomorph.R
+++ tests/testthat/test-geomorph.R
@@ -6,81 +6,42 @@
 ###
 ### -----------------------------------------------------------------------
 
+### arrayspecs --------------------------------------------------------------
 
-### procD.lm examples --------------------------------------------------------------
-
-test_that("procD.lm.examples.works", {
-  library(geomorph)
-  data(plethodon) 
-  Y.gpa <- gpagen(plethodon$land)    #GPA-alignment  
-  gdf <- geomorph.data.frame(Y.gpa, 
-                             site = plethodon$site, 
-                             species = plethodon$species) # geomorph data frame
-  
-  succeed(fit1 <- procD.lm(coords ~ species * site, 
-                   data = gdf, iter = 3, turbo = TRUE,
-                   RRPP = FALSE, print.progress = FALSE))
-  succeed(fit2 <- procD.lm(coords ~ species * site, 
-                   data = gdf, iter = 3, turbo = TRUE,
-                   RRPP = TRUE, print.progress = FALSE))
-  
-  succeed(summary(fit1))
-  succeed(summary(fit2))
-  succeed(coef(fit2))
-  succeed(coef(fit2, test = TRUE))
-  succeed(anova(fit2)) 
-  succeed(anova(fit2, effect.type = "Rsq"))
-  succeed(anova(fit2, error = c("species:site", "species:site", "Residuals")))
-  M <- Y.gpa$consensus
-  succeed(plotRefToTarget(M, fit2$GM$fitted[,,1], mag = 3))
-  succeed(plotRefToTarget(M, fit2$GM$fitted[,,20], mag = 3))
-  
-  succeed(fit.null <- procD.lm(coords ~ log(Csize) + species + site, data = gdf, 
-                       iter = 3, print.progress = FALSE, turbo = TRUE))
-          succeed(fit.full <- procD.lm(coords ~ log(Csize) + species * site, data = gdf, 
-                       iter = 3, print.progress = FALSE, turbo = TRUE))
-  succeed(anova(fit.null, fit.full, print.progress = FALSE))
-  succeed(gp <-  interaction(gdf$species, gdf$site))
-  succeed(PW <- pairwise(fit.full, groups = gp, covariate = NULL))
-  succeed(summary(PW, test.type = "dist", confidence = 0.95, stat.table = TRUE))
-  succeed(summary(PW, test.type = "dist", confidence = 0.95, stat.table = FALSE))
-  succeed(summary(PW, test.type = "var", confidence = 0.95, stat.table = TRUE))
-  succeed(summary(PW, test.type = "var", confidence = 0.95, stat.table = FALSE))
-  succeed(morphol.disparity(fit.full, groups = gp, iter = 3))
-  data(ratland)
-  rat.gpa<-gpagen(ratland)         #GPA-alignment
-  gdf <- geomorph.data.frame(rat.gpa) # geomorph data frame is easy 
-  succeed(fit <- procD.lm(coords ~ Csize, data = gdf, iter = 3, turbo = TRUE,
-                  RRPP = TRUE, print.progress = FALSE))
-  succeed(summary(fit))
-  succeed(plot(fit, type = "diagnostics"))
-  succeed(plot(fit, type = "diagnostics", outliers = TRUE))
-  succeed(plot(fit, type = "PC", pch = 19, col = "blue"))
-  succeed(plot(fit, type = "regression", 
-       predictor = gdf$Csize, reg.type = "RegScore", 
-       pch = 19, col = "green"))
-  succeed(rat.plot <- plot(fit, type = "regression", 
-                   predictor = gdf$Csize, reg.type = "RegScore", 
-                   pch = 21, bg = "yellow"))
-  succeed(preds <- shape.predictor(fit$GM$fitted, x = rat.plot$RegScore, 
-                           predmin = min(rat.plot$RegScore), 
-                           predmax = max(rat.plot$RegScore)))
-  M <- rat.gpa$consensus
-  succeed(plotRefToTarget(M, preds$predmin, mag=2))
-  succeed(plotRefToTarget(M, preds$predmax, mag=2))
-
-  data("larvalMorph")
-  Y.gpa <- gpagen(larvalMorph$tailcoords, 
-                  curves = larvalMorph$tail.sliders,
-                  ProcD = TRUE, print.progress = FALSE)
-  gdf <- geomorph.data.frame(Y.gpa, treatment = larvalMorph$treatment, 
-                             family = larvalMorph$family)
-  
-  succeed(fit <- procD.lm(coords ~ treatment/family, data = gdf, turbo = TRUE,
-                  print.progress = FALSE, iter = 3))
-  succeed(anova(fit))
-  succeed(anova(fit, error = c("treatment:family", "Residuals")))
-  
+test_that("arrayspecs.works", {
+  x <- matrix(rnorm(18), nrow = 3)
+  succeed(arrayspecs(x, 3, 2)) 
 })
 
-# Other tests that are not examples can also go here.
+### bilat.symmetry --------------------------------------------------------------
+
+test_that("bilat.symmetry1.works", {
+  data(mosquito)
+  gdf <- geomorph.data.frame(wingshape = mosquito$wingshape, 
+         ind = mosquito$ind, side = mosquito$side,
+         replicate = mosquito$replicate)
+  succeed(mosquito.sym <- bilat.symmetry(A = wingshape, ind = ind, side = side,
+         replicate = replicate, object.sym = FALSE, RRPP = TRUE, 
+         iter = 3, data = gdf))
+})
+
+## THIS ONE DOESN'T WORK.  AND IMPLIES THE HELP EXAMPLE REQUIRES TWEAKING FOR PROPER NOTATION
+test_that("bilat.symmetry2.works", {
+  data(mosquito)
+  Y.gpa <- gpagen(mosquito$wingshape)
+  succeed(mosquito.sym2 <- bilat.symmetry(A = Y.gpa, ind = mosquito$ind, 
+    side = mosquito$side, replicate = mosquito$replicate, 
+    object.sym = FALSE, RRPP = TRUE, iter = 3))
+})
+
+test_that("bilat.symmetry3.works", {
+  data(lizards)
+  gdf <- geomorph.data.frame(shape = lizards$coords, 
+    ind = lizards$ind, 
+    replicate = lizards$rep)
+  succeed(liz.sym <- bilat.symmetry(A = shape, ind = ind, rep = rep, 
+    object.sym = TRUE, land.pairs = lizards$lm.pairs, 
+    iter = 3, data = gdf, RRPP = TRUE))
+})
+
+
