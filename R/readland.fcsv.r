7c4cba3fc1575e987a4ea5fc13cf53d34f294ae3
Dean Adams <dcadams@iastate.edu>
2024-08-07 07:43:45 -0500

    fcsv

--- R/readland.fcsv.r
+++ R/readland.fcsv.r
@@ -8,12 +8,13 @@
 #' @param header_no The number of header lines in the fscv file, needed for knowing these lines are not data.
 #' @keywords IO
 #' @export
-#' @author Murat Maga and Michael Collyer
+#' @author Murat Maga, Michael Collyer and Dean Adams
 #' @return Function returns a p x 3 matrix of x, y, z coordinates for p landmarks.
 
 readland.fcsv = function (file = NULL, header_no = 3)  {
-  
-  res <- read.csv(file = file, skip = header_no, header = T)[, 1:4]
+  testfile <- scan(file=file, what="char", quote="", sep="\n", strip.white=TRUE, comment.char="\"", quiet=TRUE)
+  header_no <- length(grep("#", testfile))
+  res <- read.csv(file = file, skip = header_no, header = F)[, 1:4]
   res.ind <- res[, 1]
   res.nms <- try(
     unlist(strsplit(res.ind, "vtkMRMLMarkups")), silent = TRUE)
