4399f59fea05931a5d300c525a08f7132e0b18e2
mlcollyer <mlcollyer@gmail.com>
2024-08-07 11:08:43 +0200

    updated fscv

--- R/readland.fcsv.r
+++ R/readland.fcsv.r
@@ -4,15 +4,16 @@
 #' 
 #' This function merely extracts x, y, z coordinates from an fcsv file.  
 #'
-#' @param file the name of a *.fcsv file containing three-dimensional landmark data to be read in
+#' @param file The name of a *.fcsv file containing three-dimensional landmark data to be read in
+#' @param header_no The number of header lines in the fscv file, needed for knowing these lines are not data.
 #' @keywords IO
 #' @export
 #' @author Murat Maga and Michael Collyer
 #' @return Function returns a p x 3 matrix of x, y, z coordinates for p landmarks.
 
-readland.fcsv = function (file = NULL)  {
+readland.fcsv = function (file = NULL, header_no = 3)  {
   
-  res <- read.csv(file = file, skip = 2, header = T)[, 1:4]
+  res <- read.csv(file = file, skip = header_no, header = T)[, 1:4]
   res.ind <- res[, 1]
   res.nms <- try(
     unlist(strsplit(res.ind, "vtkMRMLMarkups")), silent = TRUE)
