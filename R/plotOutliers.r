f858a7e3a7c53c68e59c3951ba352ae6cb0e78d8
Michael Collyer <mlcollyer@gmail.com>
2026-06-30 12:33:34 -0400

    plotOutliers un-overwrite

--- R/plotOutliers.r
+++ R/plotOutliers.r
@@ -2,11 +2,19 @@
 #' 
 #' Function plots all specimens ordered by distance from the mean.
 #' 
-#' The function creates a plot of all specimens ordered by their Procrustes distance from the mean shape. 
-#' The median distance (unbroken line) and upper and lower quartiles (dashed lines) summarize the distances
-#' from the mean shape. Specimens falling above the upper quartile are plotted in red. The user may optionally 
-#' also inspect the shapes of identified outlier configurations as compared to the consensus, in order
-#' to identify digitization errors or other data issues. The addresses of all specimens are
+#' The function creates a plot of all specimens ordered by their distance from the mean shape. There
+#' is an option for using specified principal components (PC) of shape.  If all shape dimensions are used
+#' then distances are equal to Procrustes distances or distances in the tangent space of shape space that 
+#' resemble Procrustes distances, depending on whether the projection was performed with generalized 
+#' Procrustes analysis (GPA).  Once distances are calculated, a power-transformation is perfomed to 
+#' normalize the distances.  From these data, an upper limit is estimated following Tukey's box-plot rule, 
+#' as \eqn{Q_3 + 1.5 \times (Q_3 - Q_1)}, where \eqn{Q} refers to quartile.  This upper limit is back-transformed 
+#' to distance, and any distances greater than this limit are colored red.  (Note: These shapes could 
+#' be considered outliers but their red color does not mean they are necessarily outliers. )
+#' 
+#'  The user may optionally 
+#' also inspect the shapes of identified configurations as compared to the consensus, in order
+#' to identify potential digitization errors or other data issues. The addresses of all specimens are
 #' returned in the order displayed in the plot for further inspection by \code{\link{plotRefToTarget}}.
 #' 
 #' If the data have strong group structure and there is reasonable belief that the whole sample mean should not be used,
@@ -14,12 +22,15 @@
 #' 
 #' @param A A 3D array (p x k x n) containing Procrustes shape variables for a set of specimens
 #' @param groups An optional factor defining groups
+#' @param PC A single number or range of principal components that can be used to check for outliers.  If
+#' NULL, all shape dimensions are used.  This argument might be useful for investigating subtle but
+#' important aspects of shape that would not elucidate outliers in all dimensions.
 #' @param inspect.outliers A logical value indicating whether to plot outlier shape configurations as compared to the consensus
 #' @export
 #' @keywords utilities
 #' @seealso  \code{\link{gpagen}}
 #' @seealso  \code{\link{plotAllSpecimens}}
-#' @author Emma Sherratt & Antigoni Kaliontzopoulou
+#' @author Emma Sherratt, Antigoni Kaliontzopoulou, & Michael Collyer
 #' @return Function returns the landmark addresses of all specimens ordered as in the plot. If groups are used, function returns 
 #' a list structure and a plot for each level in groups.
 #' @examples
@@ -29,8 +40,9 @@
 #' newland <- plethodon$land
 #' newland[c(1,8),,2] <- newland[c(8,1),,2]
 #' newland[c(3,11),,26] <- newland[c(11,3),,2]
-#' Y<- gpagen(newland) # GPA
+#' Y <- gpagen(newland) # GPA
 #' out <- plotOutliers(Y$coords) # function returns dimnames and address 
+#' out
 #' # of all specimens ordered
 #' plotOutliers(Y$coords, inspect.outliers = TRUE) # function also produces 
 #' # plots of identified outlier specimens compared to the mean shape
@@ -38,61 +50,90 @@
 #' # example with groups
 #' plotOutliers(Y$coords, groups = plethodon$species, 
 #' inspect.outliers = TRUE)
+#' 
+#' # previous example using first three PCs of shape
+#' plotOutliers(Y$coords, groups = plethodon$species, 
+#' PC = 1:3, inspect.outliers = TRUE)
+#' 
+#' # previous example using just the first PC of shape
+#' plotOutliers(Y$coords, groups = plethodon$species, 
+#' PC = 1, inspect.outliers = TRUE)
 #'  }
-plotOutliers <- function(A, groups = NULL, inspect.outliers = FALSE){
-  if (length(dim(A))!=3){
-    stop("Data matrix not a 3D array (see 'arrayspecs').")  }
-  if(is.null(groups)){ groups = factor(rep("All Specimens",dim(A)[3]))}
+plotOutliers <- function(A, groups = NULL, 
+                         PC = NULL,
+                         inspect.outliers = FALSE){
+  if(length(dim(A)) != 3)
+    stop("\nData matrix not a 3D array (see 'arrayspecs').\n",
+         call. = FALSE)  
+  
+  if(is.null(groups))
+    groups <- factor(rep("All Specimens",dim(A)[3]))
+  
+  glen <- as.vector(table(groups))
+  if(any(glen < 3))
+    stop("\n One or more groups have fewer than 3 shapes.  Outliers cannot be deteced.\n",
+         call. = FALSE)
+  options(viewer = NULL)  # for 3D plotting
+  Ymat <- gm.prcomp(A)$x
+  if(is.null(PC)) PC <- 1:ncol(Ymat)
+  if(any(PC > ncol(Ymat)))
+    stop("\nChoose a number of PCs that does not exceed the total number of PCs\n.",
+         call. = FALSE)
+  Ymat <- as.matrix(Ymat[, PC])
+  n <- NROW(Ymat)
+  
+  if(is.null(dimnames(A)[[3]]))
+    dimnames(A)[[3]] <- rownames(Ymat) <- 1:n
+
   res <- lapply(levels(groups), function(j){
-    mn <- matrix(t(mshape(A[,,which(groups==j)])), nrow=1) 
-    A.d <- two.d.array(A[,,which(groups==j)])
-    d <- NULL
-    for(i in 1:nrow(A.d)){ d <- c(d, as.vector(dist(rbind(mn, A.d[i,])))) }
-    if(is.null(dimnames(A.d)[[1]])) { dimnames(A.d)[[1]] <- as.character(seq(1:nrow(A.d)))}
-    names(d) <- dimnames(A.d)[[1]] 
-    D <- d[order(d, decreasing=TRUE)]
-    Q <- summary(D)
-    Med <- as.numeric(summary(D)[3])
-    LL <- as.numeric(Q[2] - 1.5*(Q[5]-Q[2]))
-    UL <- as.numeric(Q[5] + 1.5*(Q[5]-Q[2]))
-    plot(D, type="p", ylab= "Procrustes Distance from Mean", pch=19, xlab="", xaxt='n', main = j)
-      abline(a=LL, b=0,lty=2,col= "blue")
-      abline(a=Med,b=0,col= "blue")
+    y <- as.matrix(center(Ymat[which(groups == j), ]))
+    D <- sqrt(rowSums(y^2))
+    D <- D[order(D, decreasing=TRUE)]
+    bc <- powerTrans(D)
+    b <- bc$transformed
+    UL <- quantile(b, 0.75) + 1.5 * IQR(b)
+    UL <- (UL * bc$opt.lambda + 1)^(1/bc$opt.lambda)
+
+    plot(D, type="p", ylab= "Distance from Mean", pch=19, xlab="", xaxt='n', main = j)
       abline(a=UL,b=0,lty=2,col= "blue")
-      text(x= nrow(A.d), y=LL, labels= "lower quartile", col = "blue", cex=0.5, adj=c(0.5, 1))
-      text(x= nrow(A.d), y=Med, labels= "median",col = "blue", cex=0.5, adj=c(0.5, -0.5))
-      text(x= nrow(A.d), y=UL, labels= "upper quartile",col = "blue", cex=0.5, adj=c(0.5, -0.5))
+      text(x= nrow(y), y=UL, labels= "upper limit",col = "blue", cex=0.5, adj=c(0.5, -0.5))
     if(any(D >= UL)) { 
-      points(D[which(D >= UL)], pch=19, col="red")
-      text(D[which(D >= UL)], labels=names(D)[which(D >= UL)], col= "red", adj=0.8, pos=4, cex=0.5)
+      ol <- D[which(D >= UL)]
+      p <- 1:length(ol)
+      points(p, ol, pch=19, col="red")
+      text(p, ol, labels = names(ol), col= "red", adj=0.8, pos=4, cex=0.5)
+    }
+      
+    if(any(D >= UL)) {   
       if(inspect.outliers == TRUE){
-        out.config <- names(D)[which(D >= UL)]
+        out.config <- names(ol)
+        Alist <- lapply(1:n, function(x) as.matrix(A[,, x]))
+        names(Alist) <- dimnames(A)[[3]]
+        OLlist <- Alist[names(ol)]
+        M <- mshape(A)
+        
         if(dim(A)[2] == 2){
-          for(oc in out.config){
-            plotRefToTarget(mshape(A), matrix(A.d[oc,], ncol=dim(A)[2], byrow=T), method="vector", label=TRUE)
-            title(main = paste("group: ", j, ", specimen: ", oc, sep=""))
+          for(i in seq_along(OLlist)){
+            plotRefToTarget(M, OLlist[[i]], method="vector", label=TRUE)
+            title(main = paste("group:", j, ", specimen:", out.config[i]))
           }
         }
+        
         if(dim(A)[2] == 3){
-          for(oc in out.config){
-            open3d()
-            plotRefToTarget(mshape(A), matrix(A.d[oc,], ncol=dim(A)[2], byrow=T), method="vector", label=TRUE)
-            bgplot3d({
-              plot.new()
-              title(main = paste("specimen: ", oc, sep=""), line = 3)
-              mtext(side = 3, paste("group: ", j, sep = ""), line = 1.5)
-            })
+          for(i in seq_along(OLlist)){
+            fig <- plotRefToTarget(M, OLlist[[i]], 
+                                   method="vector", label=TRUE)
+            fig <- fig |>
+              plotly::layout(title = paste("group:", j,
+                                   "<br>specimen:", out.config[i]))
+            print(fig)
           }
         }
       }
-
-    } else { text(D, labels=names(D), adj=c(0.5, 0.1), pos=4, cex=0.5)}
-    ordered<-match(D,d)        
-    names(ordered) <- names(D)
-    return(ordered)
+    } else { text(1:n, D, labels=names(D), adj=c(0.5, 0.1), pos=4, cex=0.5)}
+    names(D)
   })
   names(res) <- levels(groups)
-  if(length(levels(groups))==1){ res <- res$`All Specimens`}
-  return(res)
-
+  if(length(glen) == 1) res <- res$`All Specimens`
+  res
 }
